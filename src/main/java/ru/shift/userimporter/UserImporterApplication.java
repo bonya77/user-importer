0052d3c173edf068c636c64084a56a92542c2c1e
dgolovin <d.golovin@korona.net>
2026-06-15 09:48:01 +0700

    init commit

---
+++ src/main/java/ru/shift/userimporter/UserImporterApplication.java
@@ -0,0 +1,13 @@
+package ru.shift.userimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserImporterApplication.class, args);
+	}
+
+}
